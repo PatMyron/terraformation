baf6905165ee69d50e346b41b1697c0451a28500
Pat Myron <pat.myron@stedi.com>
2022-01-04 00:54:25 -0500

    general type mapping function beyond resource types

--- map/map.py
+++ map/map.py
@@ -1,11 +1,16 @@
 import requests
 cfTypes = requests.get('https://d1uauaxba7bl26.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json').json()['ResourceTypes'].keys()
 tfTypes = requests.get('https://raw.githubusercontent.com/PatMyron/terraformation/create-pull-request/patch/schemas/tf/aws.json').json()['provider_schemas']['registry.terraform.io/hashicorp/aws']['resource_schemas'].keys()
-cfMap = {}
-for cfType in cfTypes:
-  cfMap[cfType.replace('_', '').replace(':', '').lower()] = cfType
-for tfType in tfTypes:
-  try:
-    print(tfType, cfMap[tfType.replace('_', '').replace(':', '').lower()])
-  except:
-    pass
+
+
+def mapTypes(cfTypes, tfTypes):
+  cfMap = {}
+  for cfType in cfTypes:
+    cfMap[cfType.replace('_', '').replace(':', '').lower()] = cfType
+  for tfType in tfTypes:
+    try:
+      print(tfType, cfMap[tfType.replace('_', '').replace(':', '').lower()])
+    except:
+      pass
+
+mapTypes(cfTypes, tfTypes)
