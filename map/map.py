1f0a5b9a034d85e17e3b67c49d19ed4d1a926e1e
Pat Myron <PatMyron@users.noreply.github.com>
2022-06-14 23:52:48 -0700

    automatically mapping awscc resource types

--- map/map.py
+++ map/map.py
@@ -1,6 +1,6 @@
 import requests
 cfTypes = requests.get('https://d1uauaxba7bl26.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json').json()['ResourceTypes'].keys()
-tfTypes = requests.get('https://raw.githubusercontent.com/PatMyron/terraformation/create-pull-request/patch/schemas/tf/schema.json').json()['provider_schemas']['registry.terraform.io/hashicorp/aws']['resource_schemas'].keys()
+tfTypes = requests.get('https://raw.githubusercontent.com/PatMyron/terraformation/create-pull-request/patch/schemas/tf/schema.json').json()['provider_schemas']['registry.terraform.io/hashicorp/awscc']['resource_schemas'].keys()
 
 
 def mapTypes(cfTypes, tfTypes):
