04ef92b764e7d74eb02195ea0e4cb44ec047db41
Pat Myron <PatMyron@users.noreply.github.com>
2025-01-24 02:46:56 -0500

    manually map service namespace mismatches (CloudWatch, VPC, RDS, etc.)
    ~40 total resource types:
    9 aws_vpc AWS::EC2
    8 aws_cloudwatch_log AWS::Logs
    5 aws_db AWS::RDS::DB (could pick up 2 AWS::RDS)
    5 aws_cloudwatch_event AWS::Events
    5 aws_sesv2 AWS::SES
    4 aws_bedrockagent AWS::Bedrock
    3 aws_ssoadmin AWS::SSO
    
    aws_route53_resolver could pick up 5 AWS::Route53Resolver::Resolver but lost 3 AWS::Route53Resolver this primitive way

--- map/map.py
+++ map/map.py
@@ -9,7 +9,7 @@ def mapTypes(cfTypes, tfTypes):
     cfMap[cfType.replace('_', '').replace(':', '').lower()] = cfType
   for tfType in tfTypes:
     try:
-      print(tfType, cfMap[tfType.replace('_', '').replace(':', '').lower()])
+      print(tfType, cfMap[tfType.replace('aws_bedrockagent_', 'aws_bedrock_').replace('aws_ssoadmin_', 'aws_sso_').replace('aws_sesv2_', 'aws_ses_').replace('aws_cloudwatch_log_', 'aws_logs_').replace('aws_cloudwatch_event_', 'aws_events_').replace('aws_db_', 'aws_rds_db_').replace('aws_vpc_', 'aws_ec2_').replace('_', '').replace(':', '').lower()])
     except:
       pass
 
